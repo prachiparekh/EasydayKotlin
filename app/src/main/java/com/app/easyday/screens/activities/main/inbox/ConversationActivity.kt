14c786119626f380f7734db903fc91aa84b528cb
krrish977 <arupanand.cubezytech@gmail.com>
2022-10-31 09:16:47 +0530

    commit 31/10/2022

--- app/src/main/java/com/app/easyday/screens/activities/main/inbox/ConversationActivity.kt
+++ app/src/main/java/com/app/easyday/screens/activities/main/inbox/ConversationActivity.kt
@@ -2,6 +2,7 @@ package com.app.easyday.screens.activities.main.inbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.app.easyday.R
 import kotlinx.android.synthetic.main.activity_conversation.*
 
@@ -11,6 +12,12 @@ class ConversationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_conversation)
 
         iv_back.setOnClickListener { onBackPressed() }
+
+        edt_msg.setOnClickListener {
+            cam.visibility = View.GONE
+            mic.visibility = View.GONE
+            send.visibility = View.VISIBLE
+        }
     }
 
     override fun onBackPressed() {
